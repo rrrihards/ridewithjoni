22bd8d9fce147b2215f52eed838443ae609a69c9
rrrihards <r.bogatijs@gmail.com>
2025-08-21 16:59:16 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Ride With Joni</h1>
+      <p>Welcome to Joni Vollenberg’s official page!</p>
+    </div>
+  );
+}
+
+export default App;
